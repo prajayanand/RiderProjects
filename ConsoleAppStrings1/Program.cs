6a9dfeebb95ce6a2bde6115f2c2b08ebfad481be
prajayanand <PRAJAYANAND@GMAIL.COM>
2024-05-30 23:20:19 -0500

    Add files via upload

---
+++ ConsoleAppStrings1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppStrings1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+        
+            // 1: Using char array reversal
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+            Console.WriteLine(reversedString);
+
+            // 2: Using for loop
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}    
---
+++ ConsoleAppStrings2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleAppStrings2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+            
+            string reversedSentence = ReverseWords(input);
+            
+            Console.WriteLine("Reversed sentence:");
+            Console.WriteLine(reversedSentence);
+        }
+
+        static string ReverseWords(string input)
+        {
+            StringBuilder word = new StringBuilder();
+            StringBuilder result = new StringBuilder();
+            StringBuilder separator = new StringBuilder();
+            
+            bool inWord = char.IsLetterOrDigit(input[0]);
+            
+            foreach (char c in input)
+            {
+                if (IsSeparator(c))
+                {
+                    if (inWord)
+                    {
+                        result.Insert(0, word.ToString() + separator.ToString());
+                        word.Clear();
+                        separator.Clear();
+                    }
+                    separator.Append(c);
+                    inWord = false;
+                }
+                else
+                {
+                    if (!inWord)
+                    {
+                        result.Insert(0, separator.ToString());
+                        separator.Clear();
+                    }
+                    word.Append(c);
+                    inWord = true;
+                }
+            }
+            
+            if (word.Length > 0)
+            {
+                result.Insert(0, word.ToString());
+            }
+            
+            if (separator.Length > 0)
+            {
+                result.Append(separator.ToString());
+            }
+
+            return result.ToString();
+        }
+
+        static bool IsSeparator(char c)
+        {
+            return " .,;=()&[]\"'\\/!?".IndexOf(c) >= 0;
+        }
+    }
+}    
