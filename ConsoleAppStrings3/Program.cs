d847464cd6bf9d507da15f6145a9d9709a245907
prajayanand <PRAJAYANAND@GMAIL.COM>
2024-05-30 23:21:55 -0500

    Add files via upload

---
+++ ConsoleAppStrings3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppStrings3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+
+            string[] palindromes = ExtractPalindromes(input);
+
+            Console.WriteLine("Palindromes:");
+            Console.WriteLine(string.Join(", ", palindromes));
+        }
+
+        static string[] ExtractPalindromes(string input)
+        {
+            HashSet<string> uniquePalindromes = new HashSet<string>();
+            List<string> words = new List<string>();
+            char[] delimiters = { ' ', ',', '.', ';', ':', '(', ')', '&', '[', ']', '\"', '\'', '\\', '/', '!', '?' };
+
+            string[] parts = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in parts)
+            {
+                if (IsPalindrome(word))
+                {
+                    uniquePalindromes.Add(word);
+                }
+            }
+
+            words.AddRange(uniquePalindromes);
+            words.Sort();
+
+            return words.ToArray();
+        }
+
+        static bool IsPalindrome(string word)
+        {
+            int length = word.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (word[i] != word[length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}    
---
+++ ConsoleAppStrings4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleAppStrings4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a URL:");
+            string url = Console.ReadLine();
+
+            ParseUrl(url);
+        }
+
+        static void ParseUrl(string url)
+        {
+            string protocol = "";
+            string server = "";
+            string resource = "";
+
+            // Find protocol
+            int protocolEndIndex = url.IndexOf("://");
+            if (protocolEndIndex != -1)
+            {
+                protocol = url.Substring(0, protocolEndIndex);
+                url = url.Substring(protocolEndIndex + 3); // Remove protocol part from URL
+            }
+
+            // Find server and resource
+            int serverEndIndex = url.IndexOf('/');
+            if (serverEndIndex != -1)
+            {
+                server = url.Substring(0, serverEndIndex);
+                resource = url.Substring(serverEndIndex + 1); // Get resource part
+            }
+            else
+            {
+                server = url;
+            }
+
+            Console.WriteLine($"[protocol] = \"{protocol}\"");
+            Console.WriteLine($"[server] = \"{server}\"");
+            Console.WriteLine($"[resource] = \"{resource}\"");
+        }
+    }
+}
