34d9f1bd1e13c79b171cc0f0406fa35e117036a5
prajayanand <PRAJAYANAND@GMAIL.COM>
2024-05-30 23:26:41 -0500

    Add files via upload

---
+++ ConsoleAppNew1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// Byte overflow
+//What happens if we run given code - 
+// When running this code it will enter into an infinite loop. 
+// Because a byte data type can only hold values from 0 to 255.
+// Once it reaches 255, it will overflow and reset to 0 and loop the same again and again.
+// To warn, we can add if condition as a check by example : max>byte then print warning
+namespace ConsoleAppNew1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 500;
+            for (byte i = 0; i < max; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ConsoleAppNew2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleAppNew2
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            // Generate a random number between 1 and 3
+            int correctNumber = new Random().Next(1, 4);
+
+            Console.WriteLine("Guess the number between 1 and 3:");
+
+            // Read the user's guess from the console
+            int guessedNumber = int.Parse(Console.ReadLine());
+
+            // Check if the guessed number is within the valid range
+            if (guessedNumber < 1 || guessedNumber > 3)
+            {
+                Console.WriteLine("Your guess is outside the valid range.");
+            }
+            else
+            {
+                // Check if the guessed number matches the correct number
+                if (guessedNumber == correctNumber)
+                {
+                    Console.WriteLine("Congratulations! You guessed the correct number.");
+                }
+                else
+                {
+                    // Provide feedback based on whether the guess is higher or lower than the correct number
+                    if (guessedNumber < correctNumber)
+                    {
+                        Console.WriteLine("Your guess is too low.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess is too high.");
+                    }
+                }
+            }
+        }
+    }
+}
