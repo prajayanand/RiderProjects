8ce0afcf66137fc1efa3045235a618e7807b45c1
prajayanand <PRAJAYANAND@GMAIL.COM>
2024-05-30 23:17:50 -0500

    Add files via upload

---
+++ ConsoleAppArrays5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleAppArray5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the array of integers (space separated):");
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split(' ');
+            int[] array = new int[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                array[i] = int.Parse(inputArray[i]);
+            }
+
+            int[] longestSequence = FindLongestSequenceOfEqualElements(array);
+
+            Console.WriteLine("Longest sequence of equal elements:");
+            Console.WriteLine(string.Join(" ", longestSequence));
+        }
+
+        static int[] FindLongestSequenceOfEqualElements(int[] array)
+        {
+            int maxLength = 1;
+            int currentLength = 1;
+            int maxElement = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    currentLength = 1;
+                }
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    maxElement = array[i];
+                }
+            }
+
+            int[] longestSequence = new int[maxLength];
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSequence[i] = maxElement;
+            }
+
+            return longestSequence;
+        }
+    }
+}    
---
+++ ConsoleAppArrays6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleAppArrays6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the array of integers (space separated):");
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split(' ');
+            int[] array = new int[inputArray.Length];
+            
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                array[i] = int.Parse(inputArray[i]);
+            }
+
+            int mostFrequent = FindMostFrequentNumber(array);
+            
+            Console.WriteLine($"The number {mostFrequent} is the most frequent.");
+        }
+
+        static int FindMostFrequentNumber(int[] array)
+        {
+            int[] uniqueNumbers = new int[array.Length];
+            int[] frequencies = new int[array.Length];
+            int uniqueCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int num = array[i];
+                int index = Array.IndexOf(uniqueNumbers, num, 0, uniqueCount);
+
+                if (index == -1)
+                {
+                    uniqueNumbers[uniqueCount] = num;
+                    frequencies[uniqueCount] = 1;
+                    uniqueCount++;
+                }
+                else
+                {
+                    frequencies[index]++;
+                }
+            }
+
+            int maxFrequency = 0;
+            int mostFrequent = uniqueNumbers[0];
+
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                if (frequencies[i] > maxFrequency)
+                {
+                    maxFrequency = frequencies[i];
+                    mostFrequent = uniqueNumbers[i];
+                }
+            }
+
+            return mostFrequent;
+        }
+    }
+}    
